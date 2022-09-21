aa77591a3e9fce44d74171902cef3b58b098862a
Turtle <emanuelsadhot@gmail.com>
2022-09-21 12:48:01 -0300

    update responsive

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -6,7 +6,7 @@ import Section from './Section';
 export default function Main() {
   return (
     <main>
-      <div className="flex pt-[70px] gap-[140px]">
+      <div className="flex pt-[40px] xl:pt-[70px] gap-[140px]">
         <div className="pl-[20px] flex-col text-white xl:pl-[300px]">
           <div>
             <h1 className="text-white text-[55px] xl:text-[48px] hover:scale-105 duration-500 hidden xl:flex">
